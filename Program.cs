56f438a8c64b1b7269c9f8723f81ab16ce790289
aurel026362 <47327848+aurel026362@users.noreply.github.com>
2019-02-07 23:10:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ocp o_c_p = new ocp();
+            lcp l = new lcp();
+            ISP isp = new ISP();
+        }
+    }
+}
---
+++ lcp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    //ICP
+
+    public interface IFlying{
+         void Fly();
+    }
+
+    public interface ISwimming
+    {
+        void Swim();
+    }
+
+    public interface IRunning
+    {
+        void Run();
+    }
+
+    public class Horse : IRunning
+    {
+        public void Run()
+        {
+
+        }
+    }
+
+    public class Human : IRunning, ISwimming
+    {
+        public void Run()
+        {
+            Console.WriteLine("Human run");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Human swim");
+        }
+    }
+
+    public class Pelican:IFlying, ISwimming
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Pelican fly");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Pelican Swim");
+        }
+    }
+
+    class lcp
+    {
+        Pelican pelican = new Pelican();
+        Human human = new Human();
+        Horse horse = new Horse();
+    }
+}
