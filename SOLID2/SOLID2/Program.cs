f771890bb90b1e16a3e7700886cd50af0cfec1d7
aurel026362 <aurel.starciuc@amdaris.com>
2019-02-08 09:26:36 +0200

    lesson2

---
+++ SOLID2/SOLID2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
